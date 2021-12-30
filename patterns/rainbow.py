a277868c52d7c33afc2fa1402b61549b5d1518dd
sparchatus <8504034+sparchatus@users.noreply.github.com>
2021-12-30 16:08:21 +0100

    Create rainbow.py

---
+++ patterns/rainbow.py
@@ -0,0 +1,32 @@
+from pattern import Pattern1D
+import numpy as np
+
+class Pattern(Pattern1D):
+
+    def __init__(self, n):
+        super().__init__(n)
+        self.colors = np.zeros((n, 3))
+        self.colors[0] = 255
+        self.head = 0
+        self.interval = 10
+        self.state = 0
+
+    def draw(self, t):
+        if state == 0:
+          self.color[0] -= 1
+          self.color[1] += 1
+        elif state == 1:
+          self.color[1] -= 1
+          self.color[2] += 1
+        else:
+          self.color[2] -= 1
+          self.color[0] += 1
+        
+        if self.color[self.state] == 0:
+          self.state = (self.state + 1) % 3
+          
+        self.colors *= 0.9
+        self.colors[self.head] = self.color
+        self.head = (self.head + 1) % self.n
+        
+        return self.colors.astype(np.int)
