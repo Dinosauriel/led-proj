cf31eed88b7f85b1c215b1dab4d9e1a29b2e881c
Aurel Feer <aurel.feer@gmail.com>
2021-12-30 14:25:46 +0000

    improve snake

--- patterns/snake.py
+++ patterns/snake.py
@@ -5,16 +5,15 @@ class Pattern(Pattern1D):
 
     def __init__(self, n):
         super().__init__(n)
-        self.golden = np.array([184,134,11]) * 0.3
-        # self.colors = np.broadcast_to(self.golden, (n, 3))
         self.colors = np.zeros((n, 3))
         self.head = 0
-        self.interval = 100
-        self.factors = np.random.uniform(low=0.0, high=1., size=(self.n, 1))
-        self.last = 0
+        self.interval = 20
 
     def draw(self, t):
-        self.colors *= 0.7
-        self.colors[self.head] = self.golden
+        if self.head == 0:
+            self.color = np.random.uniform(0.0, 255., size=3)
+
+        self.colors *= 0.9
+        self.colors[self.head] = self.color
         self.head = (self.head + 1) % self.n
         return self.colors.astype(np.int)
