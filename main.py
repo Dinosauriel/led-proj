674a0bb36ba1c0c3219652d9bff4e996a6f54564
Aurel Feer <aurel.feer@gmail.com>
2022-12-11 22:30:02 +0100

    add rotate pattern

--- main.py
+++ main.py
@@ -1,87 +1,75 @@
-import argparse
-import time
-import config
-import neopixel
-import board
-import importlib
-from controls.controls import XboxController
-from pattern_lib.clear import *
-from pattern_lib.color_palette import *
-from pattern_lib.glitter import *
-from pattern_lib.rainbow import *
-from pattern_lib.snake import *
-from pattern_lib.uniform import *
+import os
+import select
 
-def get_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--pattern", help="name of the pattern to be displayed")
-    parser.add_argument("-c", "--calibrate", choices=["server", "client"], help="mode to use for calibration. e.g. 'server' for raspberry pi, 'client' for laptop taking photos")
-    parser.add_argument("-g", "--gencoords", help="generate the coords.csv file from image folder")
-    return parser.parse_args()
+# event interface:
+# https://www.kernel.org/doc/html/latest/input/input.html#event-interface
+
+# event codes:
+# https://www.kernel.org/doc/html/latest/input/event-codes.html#input-event-codes
+# https://github.com/torvalds/linux/blob/master/include/uapi/linux/input-event-codes.h
+
+class XboxController():
+    EV_KEY = 0x01
+
+    EV_VALUE_KEYPRESS = 0x1
+    EV_VALUE_KEYRELEASE = 0x0
+
+    XBOX_A = 0x130
+    XBOX_B = 0x131
+    XBOX_Y = 0x134
+    XBOX_X = 0x133
+
+    XBOX_HOME = 0x13c
+
+    def __init__(self) -> None:
+        self.path = "/dev/input/event0"
+        self.is_connected = False
+        self.reconnection_count = 0
+        self.open_controller()
+
+    def open_controller(self):
+        try:
+            self.fd = os.open(self.path, os.O_RDONLY | os.O_NONBLOCK)
+            self.is_connected = True
+        except FileNotFoundError as e:
+            print("didnt find controller file:", self.path)
 
 
+    def poll_buttons(self):
+        buttons = {
+            self.XBOX_A: False,
+            self.XBOX_B: False,
+            self.XBOX_Y: False,
+            self.XBOX_X: False,
 
-def main():
-    args = get_args()
-    controller = XboxController()
+            self.XBOX_HOME: False
+        }
+        if not self.is_connected:
+            self.reconnection_count += 1
+            if self.reconnection_count > 100:
+                self.reconnection_count = 0
+                self.open_controller()
+            return buttons
 
-    if args.calibrate == "server":
-        import calibrate_server
-        print(f"running calibration as {args.calibrate}")
-        calibrate_server.calibrate_server()
-        return
-    elif args.calibrate == "client":
-        import calibrate_client
-        print(f"running calibration as {args.calibrate}")
-        calibrate_client.calibrate_client()
-        return
-
-    if args.pattern != None:
-        patterns = [
-            PatternClear,
-            PatternColorPalette,
-            PatternGlitter,
-            PatternRainbow,
-            PatternSnake,
-            PatternUniform
-        ]
-        pattern_i = 0
-
-        # print(f"running pattern {args.pattern}")
-        pixels = neopixel.NeoPixel(board.D18, config.NUM_LEDS, auto_write=False, pixel_order=neopixel.RGB, brightness=1.0)
-
-        # try:
-        #     mod = importlib.import_module("patterns." + args.pattern)
-        # except:
-        #     print(f"couln't find pattern \"{args.pattern}\". make sure \"patterns/{args.pattern}.py\" exists")
-        #     return
-
-        pattern = patterns[pattern_i](config.NUM_LEDS)
-        last_frame = 0
         while True:
+            ready, _, _ = select.select([self.fd], [], [], 0)
+            if not ready:
+                break
+            try:
+                buf = os.read(self.fd, 16)
+            except OSError as e:
+                os.close(self.fd)
+                self.is_connected = False
+                print("controller not available")
+                break
 
-            buttons = controller.poll_buttons()
+            # sec = int.from_bytes(buf[:4], "little")
+            e_type = int.from_bytes(buf[8:10], "little")
+            e_code = int.from_bytes(buf[10:12], "little")
+            e_value = int.from_bytes(buf[12:], "little")
 
-            if buttons[controller.XBOX_A]:
-                pattern_i = (pattern_i + 1) % len(patterns)
-                pattern = patterns[pattern_i](config.NUM_LEDS)
-                last_frame = 0
-                print("pattern: ", pattern.__class__.__name__)
-            if buttons[controller.XBOX_HOME]:
-                print("turning off")
-                pixels.fill((0, 0, 0))
-                pixels.show()
-                exit()
-
-            t = int(time.time() * 1000)
-            if t - last_frame >= pattern.interval:
-                last_frame = t
-                pixels[:] = pattern.draw(t)
-                pixels.show()
-            time.sleep(0.0005)
-
-    print("no command issued. exiting")
-    return
-
-if __name__ == "__main__":
-    main()
+            if e_type == self.EV_KEY:
+                if e_value == self.EV_VALUE_KEYPRESS:
+                    buttons[e_code] = True
+                    # print(f"type: {e_type:08x}, code: {e_code:04x}, value: {e_value:04x}")
+        return buttons
---
+++ pattern_lib/rotate.py
@@ -0,0 +1,34 @@
+from pattern import Pattern1D
+import numpy as np
+
+def rotate(color, speed):
+    # print("rotate", color, ", ", speed)
+    zero = 0
+    for i in range(len(color)):
+        if color[i] == 0 and color[(i + 1) % 3] != 0:
+            zero = i
+
+    prev = (zero - 1) % 3
+    next = (zero + 1) % 3
+
+    if color[prev] < 196:
+        color[prev] = min(color[prev] + 3 * speed, 196)
+    else:
+        color[next] = max(color[next] - 3 * speed, 0)
+    return color
+
+
+class PatternRotate(Pattern1D):
+
+    def __init__(self, n):
+        super().__init__(n)
+        self.colors = np.zeros((n, 3))
+        for i in range(n):
+            self.colors[i][0] = 196
+        self.interval = 40
+        self.speed = np.random.uniform(low=0.2, high=1., size=(self.n, 1))
+
+    def draw(self, t):
+        for i in range(self.n):
+            self.colors[i] = rotate(self.colors[i], self.speed[i])
+        return self.colors.astype(np.int)
